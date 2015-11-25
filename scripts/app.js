10c21926c3b34f1266cedca99ffcfb65e55e9c03
Rica Zhang <ricagz@yahoo.com>
2015-11-25 11:05:41 -0800

    Updated styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,20 @@
-/* slide panel toggle */
-$('.card').on('click', function(event){
-	event.preventDefault();
-	$('.cd-panel').toggleClass('is-visible');
-});
-$('.cd-panel').on('click', function(event){
-	if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
-		$('.cd-panel').removeClass('is-visible');
+$(function() {
+	/* slide panel toggle */
+	$('.project-name').on('click', function(event){
 		event.preventDefault();
-	}
-});
+		if ($(this).hasClass('add-card')) {
+			return;
+		}
+		$('.cd-panel').toggleClass('is-visible');
+	});
+	$('.cd-panel').on('click', function(event){
+		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel').removeClass('is-visible');
+			event.preventDefault();
+		}
+	});
+
+	$('add-card').click(function() {
+		$('#main-section').append();
+	});
+})
